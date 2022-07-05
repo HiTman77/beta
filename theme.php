e4e88a8d02205f1addbf445b9d5a0afcd2c6ab83
HiTman77 <doug@kadabra.co.za>
2022-07-05 18:41:02 +0200

    1

--- theme.php
+++ theme.php
@@ -33,9 +33,7 @@ if($Wcms->currentPage == $Wcms->get('config', 'login')) {
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 <link href="https://fonts.googleapis.com/css?family=Lato:300,400&display=swap" rel="stylesheet">
 <?= $Wcms->css() ?>
- 
-<link rel="stylesheet" href="<?= $Wcms->asset('css/style.css') ?>?v12">
-
+<link rel="stylesheet" href="<?= $Wcms->asset('css/style.css') ?>?v13">
 <script>var page=<?=json_encode($Wcms->currentPage)?>,heading=<?=json_encode($heading)?>,subtitle=<?=json_encode($subtitle)?>,image=<?=json_encode($page_image)?>,height=<?=json_encode($height)?>,type=<?=json_encode($type)?>,loggedIn=<?=json_encode($Wcms->loggedIn)?>;</script>
 <style>.parallax{height:<?=htmlentities($height)?>vh;}</style>
 </head>
